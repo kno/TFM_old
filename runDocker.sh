41e7af62cd874a2d9363dc4063b72bd775305dc9
kno <linsms@gmail.com>
2018-10-27 10:51:27 +0200

    Base TFM2

--- runDocker.sh
+++ runDocker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nvidia-docker run -it --rm -e DISPLAY=:0 -v $HOME/.Xauthority:/root/.Xauthority -v /home/kno/Documentos/Master/TFM/TFM/catkin_ws:/root/catkin_ws -device=/dev/input/js0:/dev/input/js0 --net host --name ros --env QT_X11_NO_MITSHM=1 linsms/gazebo-ros-indigo-ardrone:working
+nvidia-docker run -it --rm -e DISPLAY=:0 -v $HOME/.Xauthority:/root/.Xauthority -v /home/kno/Documentos/Master/TFM/TFM/catkin_ws:/root/catkin_ws -device=/dev/input/js0:/dev/input/js0 --net host --name ros --env QT_X11_NO_MITSHM=1 linsms/gazebo-ros-indigo-ardrone:snapshot
