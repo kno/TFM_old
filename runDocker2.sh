7790ae8be122d8474d0a457216574086710c670f
kno <linsms@gmail.com>
2019-01-30 19:59:46 +0100

    Added new docker with gpu script

--- runDocker2.sh
+++ runDocker2.sh
@@ -6,7 +6,8 @@ nvidia-docker run -it --rm \
         -v $HOME/.Xauthority:/root/.Xauthority \
         -v /home/kno/Documentos/Master/TFM/TFM/catkin_ws:/root/catkin_ws \
         -device=/dev/input/js0:/dev/input/js0 \
-        --name ros \
+        --net host \
+        --name ros2 \
         --env QT_X11_NO_MITSHM=1 \
         -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-        linsms/gazebo-ros-indigo-ardrone
+        linsms/tfm:0.2
